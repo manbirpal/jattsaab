5506ce97d96c909e0ffef48f01c336adfc205492
manbirpal <34916158+manbirpal@users.noreply.github.com>
2019-12-31 12:05:53 +0530

    Update android.py

--- userbot/plugins/android.py
+++ userbot/plugins/android.py
@@ -3,7 +3,7 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
-""" Userbot module containing commands related to android"""
+""".android Userbot module containing commands related to android"""
 
 import re
 from requests import get
