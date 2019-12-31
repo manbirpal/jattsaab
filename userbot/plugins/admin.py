f3c94994b43f6450c81213998a46d1eb5353ce32
manbirpal <34916158+manbirpal@users.noreply.github.com>
2019-12-31 10:48:02 +0530

    Update admin.py

--- userbot/plugins/admin.py
+++ userbot/plugins/admin.py
@@ -28,7 +28,7 @@ from userbot.utils import register, errors_handler
 # =================== CONSTANT ===================
 PP_TOO_SMOL = "`The image is too small`"
 PP_ERROR = "`Failure while processing the image`"
-NO_ADMIN = "`I am not an admin nub nibba!`"
+NO_ADMIN = "`I am not an admin!`"
 NO_PERM = "`I don't have sufficient permissions! This is so sed. Alexa play despacito`"
 NO_SQL = "`Running on Non-SQL mode!`"
 
