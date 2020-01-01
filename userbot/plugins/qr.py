be581b1a2896848a05f7054bddfff6aee622a22a
manbirpal <34916158+manbirpal@users.noreply.github.com>
2020-01-01 11:29:22 +0530

    Create qr.py

---
+++ userbot/plugins/qr.py
@@ -0,0 +1,39 @@
+"""COMMAND : .qr"""
+
+from telethon import events
+
+import asyncio
+
+
+
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 30
+    
+
+    animation_ttl = range(0, 103)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "qr":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+                
+            "DOWNLOAD PAYTM QR CODE :- https://imgur.com/xKjNO0w",
+        ]
+
+        for i in animation_ttl:
+
+
+            await event.edit(animation_chars[i % 103])
